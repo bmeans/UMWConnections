bab27427c9b7de9682fd3c6f8c688b92005f9454
Danielle Lambert <dlambert@mail.umw.edu>
2012-02-25 15:58:20 -0500

    added php files

---
+++ xhtml-css/advanced_search.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Single 2 Mingle</title>
+<style type="text/css" media="screen">
+@import url("css/layout.css");
+</style>
+</head>
+<body>
+<!--layout starts-->
+<div id="layout">
+  <!--header starts-->
+  <div id="header">
+    <!--menu starts-->
+    <div id="menu"> <span class="logo"><a href="#"><img src="images/logo.gif" alt="" /></a></span>
+      <p>Fusce tristique, nisl vel gravida venenatis, risus magna eleifend pede, id bibendum mauris metus et erat.</p>
+      <ul>
+        <li class="first"><a href="index.html">home</a></li>
+        <li><a href="about_us.html" class="about_us.html">about us</a></li>
+        <li><a href="register.php">create account</a></li>
+        <li><a href="my_profile.php">my profile</a></li>
+        <li><a class="current">advanced search</a></li>
+        <li><a href="support.html">support</a></li>
+        <li><a href="contact_us.php">contact Us</a></li>
+      </ul>
+    </div>
+    <!--menu ends-->
+    <div id="banner_inner">
+      <div class="find_love">
+        <h2><img src="images/find_your_love.gif" alt="" /></h2>
+        <!--form container starts-->
+        <div class="form_container">
+          <form action="" method="get">
+            <fieldset>
+            <div class="search_row">
+              <div class="search_column_1">
+                <label>I am a</label>
+              </div>
+              <div class="search_column_2">
+                <select class="gender">
+                  <option>Male</option>
+                </select>
+                <label class="seeking">Seeking a</label>
+                <select class="gender">
+                  <option>Female</option>
+                </select>
+              </div>
+            </div>
+            <div class="search_row">
+              <div class="search_column_1">
+                <label>Looking for a</label>
+              </div>
+              <div class="search_column_2">
+                <select class="date">
+                  <option>Date</option>
+                </select>
+              </div>
+            </div>
+            <div class="search_row">
+              <div class="search_column_1">
+                <label>I was born</label>
+              </div>
+              <div class="search_column_2">
+                <select class="dob">
+                  <option>Month</option>
+                </select>
+                <select class="dob">
+                  <option>Date</option>
+                </select>
+                <select class="dob">
+                  <option>Year</option>
+                </select>
+              </div>
+            </div>
+            <div class="search_row">
+              <div class="search_column_1">
+                <label>By Profile ID</label>
+              </div>
+              <div class="search_column_2">
+                <input type="text" name="" value="" />
+                <label class="check">With Photo</label>
+                <input type="checkbox" name="" value="" class="checkbox"/>
+              </div>
+            </div>
+            <div class="search_row last">
+              <div class="search_column_1">&nbsp;</div>
+              <div class="search_column_2">
+                <input type="image" src="images/find_btn.gif" class="search_btn"/>
+              </div>
+            </div>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+      <!--form container ends-->
+    </div>
+  </div>
+  <!--header ends-->
+  <!--body container starts-->
+  <div id="body_container">
+    <!--left container starts-->
+    <div id="left_container">
+      <div style="padding:20px 15px 30px 15px;">
+      <h1><span>Services </span>for you</h1>
+      <div><strong> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin  sed odio et ante adipiscing lobortis. Quisque eleifend, arcu a dictum  varius, risus neque venenatis arcu, a semper massa mi eget ipsum. </strong> <br />
+        <br />
+        Lorem  ipsum dolor sit amet, consectetuer adipiscing elit. Proin sed odio et  ante adipiscing lobortis. Quisque eleifend, arcu a dictum varius, risus  neque venenatis arcu, a semper massa mi eget ipsum. Proin sed odio et  ante adipiscing lobortis. Lorem ipsum dolor sit amet, consectetuer  adipiscing elit. Proin sed odio et ante adipiscing lobortis. Quisque  eleifend, arcu a dictum varius, risus neque venenatis arcu, a semper  massa mi eget ipsum. Proin sed odio et ante adipiscing lobortis.<br />
+        <br />
+        Quisque  eleifend, arcu a dictum varius, risus neque venenatis arcu, a semper  massa mi eget ipsum. Proin sed odio et ante adipiscing lobortis. Proin  sed odio et ante adipiscing lobortis. Lorem ipsum dolor sit amet,  consectetuer adipiscing elit. Proin sed odio et ante adipiscing  lobortis. </div>
+      <div class="clear"></div>
+      <div class="clear"></div>
+      <div class="servicecolumnzone">
+        <div class="servicecolumn1">
+          <div>
+            <h5 class="inner"> Service 1</h5>
+            <img src="images/ico1.png" alt="" class="abouticon" />Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin sed  odio et ante adipiscing lobortis. Quisque eleifend, arcu a dictum  varius, </div>
+          <div class="clear"></div>
+        </div>
+        <div class="servicecolumn2">
+          <div>
+            <h5 class="inner">Service 2</h5>
+            <img src="images/ico2.png" alt="" width="65" height="65" class="abouticon" />Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin sed  odio et ante adipiscing lobortis. Quisque eleifend, arcu a dictum  varius, </div>
+          <div class="clear"></div>
+        </div>
+        <div class="clear"></div>
+        <div class="servicecolumn1">
+          <div>
+            <h5 class="inner">Service 3 </h5>
+            <img src="images/ico3.png" alt="" width="65" height="65" class="abouticon" />Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin sed  odio et ante adipiscing lobortis. Quisque eleifend, arcu a dictum  varius, </div>
+          <div class="clear"></div>
+        </div>
+        <div class="servicecolumn2">
+          <div>
+            <h5 class="inner">Service 4 </h5>
+            <img src="images/ico4.png" alt="" width="65" height="65" class="abouticon" />Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin sed  odio et ante adipiscing lobortis. Quisque eleifend, arcu a dictum  varius, </div>
+          <div class="clear"></div>
+        </div>
+        <div class="clear"></div>
+        <div class="servicecolumn1">
+          <div>
+            <h5 class="inner">Service 5 </h5>
+            <img src="images/ico5.png" alt="" width="65" height="65" class="abouticon" />Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin sed  odio et ante adipiscing lobortis. Quisque eleifend, arcu a dictum  varius, </div>
+          <div class="clear"></div>
+        </div>
+        <div class="servicecolumn2">
+          <div>
+            <h5 class="inner">Service 6 </h5>
+            <img src="images/ico6.png" alt="" width="65" height="65" class="abouticon" />Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin sed  odio et ante adipiscing lobortis. Quisque eleifend, arcu a dictum  varius, </div>
+          <div class="clear"></div>
+        </div>
+        <div class="clear"></div>
+      </div>
+      <div style="padding-top:10px;">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin sed  odio et ante adipiscing lobortis. Quisque eleifend, arcu a dictum  varius, risus neque venenatis arcu, a semper massa mi eget ipsum. Proin  sed odio et ante adipiscing lobortis. Lorem ipsum dolor sit amet,  consectetuer adipiscing elit. Proin sed odio et ante adipiscing  lobortis. Quisque eleifend, arcu a dictum varius, risus neque venenatis  arcu, a semper massa mi eget ipsum. Proin sed odio et ante adipiscing  lobortis.</div>
+    </div>
+      <div class="clear"></div>
+    </div>
+    <!--left container ends-->
+    <!--right container starts-->
+    <div id="right_container">
+      <!--recently profiles starts-->
+      <h5>Recently Added Profiles</h5>
+      <div id="profile_container">
+        <div class="profile_box">
+          <div class="top_curve">
+            <div class="profile_content">
+              <div class="pro_photo"><a href="#"><img src="images/pic_1.gif" alt="" /></a></div>
+              <div class="outer">
+                <div class="pro_detail_box">
+                  <div class="pro_top_curve">
+                    <div class="pro_detail_content">
+                      <div class="row">
+                        <p class="left">Name :</p>
+                        <p class="right">Jenifer</p>
+                      </div>
+                      <div class="row">
+                        <p class="left">Age :</p>
+                        <p class="right">23 Years</p>
+                      </div>
+                      <div class="row">
+                        <p class="left">Location :</p>
+                        <p class="right">South Korea</p>
+                      </div>
+                      <div class="row last">
+                        <p class="left">Likes :</p>
+                        <p class="right">Duis cursus tortor elit</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="profile_box">
+          <div class="top_curve">
+            <div class="profile_content">
+              <div class="pro_photo"><a href="#"><img src="images/pic_2.gif" alt="" /></a></div>
+              <div class="outer">
+                <div class="pro_detail_box">
+                  <div class="pro_top_curve">
+                    <div class="pro_detail_content">
+                      <div class="row">
+                        <p class="left">Name :</p>
+                        <p class="right">David Martin</p>
+                      </div>
+                      <div class="row">
+                        <p class="left">Age :</p>
+                        <p class="right">28 Years</p>
+                      </div>
+                      <div class="row">
+                        <p class="left">Location :</p>
+                        <p class="right">England</p>
+                      </div>
+                      <div class="row last">
+                        <p class="left">Likes :</p>
+                        <p class="right">Duis cursus tortor elit</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="profile_box">
+          <div class="top_curve">
+            <div class="profile_content">
+              <div class="pro_photo"><a href="#"><img src="images/pic_3.gif" alt="" /></a></div>
+              <div class="outer">
+                <div class="pro_detail_box">
+                  <div class="pro_top_curve">
+                    <div class="pro_detail_content">
+                      <div class="row">
+                        <p class="left">Name :</p>
+                        <p class="right">Shakira</p>
+                      </div>
+                      <div class="row">
+                        <p class="left">Age :</p>
+                        <p class="right">25 Years</p>
+                      </div>
+                      <div class="row">
+                        <p class="left">Location :</p>
+                        <p class="right">Argentina</p>
+                      </div>
+                      <div class="row last">
+                        <p class="left">Likes :</p>
+                        <p class="right">Duis cursus tortor elit</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="profile_box last">
+          <div class="top_curve">
+            <div class="profile_content">
+              <div class="pro_photo"><a href="#"><img src="images/pic_4.gif" alt="" /></a></div>
+              <div class="outer">
+                <div class="pro_detail_box">
+                  <div class="pro_top_curve">
+                    <div class="pro_detail_content">
+                      <div class="row">
+                        <p class="left">Name :</p>
+                        <p class="right">Jenifer</p>
+                      </div>
+                      <div class="row">
+                        <p class="left">Age :</p>
+                        <p class="right">23 Years</p>
+                      </div>
+                      <div class="row">
+                        <p class="left">Location :</p>
+                        <p class="right">Argentina</p>
+                      </div>
+                      <div class="row last">
+                        <p class="left">Likes :</p>
+                        <p class="right">Duis cursus tortor elit</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--recently profiles starts-->
+    </div>
+    <!--right container ends-->
+  </div>
+  <!--body container ends-->
+  <!--footer starts-->
+  <div id="footer"> <span class="footer_logo"><a href="#"><img src="images/single_2_mingle.gif" alt="" /></a></span>
+    <div class="footer_link">
+        <ul style="color:#FFF;">
+        Copyright (c) Sitename.com. All rights reserved. Design by Stylish <a style="color:#FFF; text-decoration:underline;" href="http://www.stylishtemplate.com">Website Templates</a>.
+      </ul>
+    </div>
+  </div>
+  <!--footer ends-->
+</div>
+<!--layout ends-->
+</body>
+</html>
---
+++ xhtml-css/register.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>UMW Connections</title>
+<style type="text/css" media="screen">
+@import url("css/layout.css");
+</style>
+</head>
+<body>
+<!--layout starts-->
+<div id="layout">
+  <!--header starts-->
+  <div id="header">
+    <!--menu starts-->
+    <div id="menu"> <span class="logo"><a href="#"><img src="images/logo.gif" alt="" /></a></span>
+      <p>Fusce tristique, nisl vel gravida venenatis, risus magna eleifend pede, id bibendum mauris metus et erat.</p>
+      <ul>
+        <li class="first"><a href="index.html">home</a></li>
+        <li><a href="about_us.html">about us</a></li>
+        <li><a class="current">create account</a></li>
+        <li><a href="my_profile.php">my profile</a></li>
+        <li><a href="advanced_search.php">advanced search</a></li>
+        <li><a href="support.html">support</a></li>
+        <li><a href="contact_us.php">contact Us</a></li>
+      </ul>
+    </div>
+    <!--menu ends-->
+    <div id="banner_inner">
+      <div class="find_love">
+        <h2><img src="images/find_your_love.gif" alt="" /></h2>
+        <!--form container starts-->
+        <div class="form_container">
+          <form action="" method="get">
+            <fieldset>
+            <div class="search_row">
+              <div class="search_column_1">
+                <label>I am a</label>
+              </div>
+              <div class="search_column_2">
+                <select class="gender">
+                  <option>Male</option>
+				  <option>Female</option>
+                </select>
+                <label class="seeking">Seeking a</label>
+                <select class="gender">
+                  <option>Female</option>
+				  <option>Male</option>
+				  <option>No preference</option>
+                </select>
+              </div>
+            </div>
+            <div class="search_row">
+              <div class="search_column_1">
+                <label>Looking for a</label>
+              </div>
+              <div class="search_column_2">
+                <select class="date">
+                  <option>Date</option>
+				  <option>Relationship</option>
+				<option>Study Group</option>
+				<option>Sports Activities</option>
+				<option>Friendship</option>
+                </select>
+              </div>
+            </div>
+            <div class="search_row">
+              <div class="search_column_1">
+                <label>Looking for</label>
+              </div>
+              <div class="search_column_2">
+                <select class="studentYear">
+                  <option>Freshmen</option>
+				  <option>Sophomore</option>
+				  <option>Junior</option>
+				  <option>Senior</option>
+				  <option>Professor</option>
+				  <option>N/A</option>
+				  <option>No Preference</option>
+                </select>
+				
+                
+              </div>
+            </div>
+            <div class="search_row">
+              <div class="search_column_1">
+                <label>Name</label>
+              </div>
+              <div class="search_column_2">
+                <input type="text" name="" value="" />
+                <label class="check">With Photo</label>
+                <input type="checkbox" name="" value="" class="checkbox"/>
+              </div>
+            </div>
+            <div class="search_row last">
+              <div class="search_column_1">&nbsp;</div>
+              <div class="search_column_2">
+                <input type="image" src="images/find_btn.gif" class="search_btn"/>
+              </div>
+            </div>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+      <!--form container ends-->
+    </div>
+  </div>
+  <!--header ends-->
+  <!--body container starts-->
+  <div id="body_container">
+    <!--left container starts-->
+	<!--registration form starts-->
+    <div id="left_container">
+     <div style="padding:20px 15px 30px 35px;">
+      <h1><span>Register </span></h1>
+      <div style="float:right; width:auto"><img src="images/ico-terms.png" alt="" /></div>
+      <div> <strong> <br />
+	  <form method="post" action="login.php">
+				<label for="email">Email address:</label>
+				<input type="text" id="email" name="email" /><br />
+				<label for="pw">Password:</label>
+				<input type="password" id="pw" name="pw" /><br />
+				<label for ="confirmPW">Confirm Password:</label>
+				<input type = "password" id="confirmPW" name = "confirmPW"/><br/>
+				<input type="submit" value="Create" name="submit" />
+			</form>
+        <br />
+       
+        <strong>Your Consent</strong><br />
+        By using our site, you consent to our <a href="#" target="_blank">websites privacy policy</a>.<br />
+        <br />
+        <strong>Changes to our Privacy Policy</strong><br />
+        If we decide to change our privacy policy, we will post those changes on this page. <br />
+        <br />
+        <strong>Contacting Us</strong> <br />
+        If there are any questions regarding this privacy policy you may contact us using the information below. <br />
+        <br />
+        MYCOMPANY.COM<br />
+        123 ADDRESS STREET<br />
+        MY CITY, MYSTATE 76543<br />
+        USA<br />
+            EMAIL@MYCOMPANY.COM<br /><hr noshade="noshade" size="1" color="#ccc" /><small>Privacy Policy 
+
+prepared by <a href="http://www.dreamtemplate.com" alt="Website 
+
+Templates">DreamTemplate</a></small> </div>
+    </div>
+    </div>
+    <!--left container ends-->
+    <!--right container starts-->
+    <div id="right_container">
+      <!--recently profiles starts-->
+      <h5>Recently Added Profiles</h5>
+      <div id="profile_container">
+        <div class="profile_box">
+          <div class="top_curve">
+            <div class="profile_content">
+              <div class="pro_photo"><a href="#"><img src="images/pic_1.gif" alt="" /></a></div>
+              <div class="outer">
+                <div class="pro_detail_box">
+                  <div class="pro_top_curve">
+                    <div class="pro_detail_content">
+                      <div class="row">
+                        <p class="left">Name :</p>
+                        <p class="right">Jenifer</p>
+                      </div>
+                      <div class="row">
+                        <p class="left">Age :</p>
+                        <p class="right">23 Years</p>
+                      </div>
+                      <div class="row">
+                        <p class="left">Location :</p>
+                        <p class="right">South Korea</p>
+                      </div>
+                      <div class="row last">
+                        <p class="left">Likes :</p>
+                        <p class="right">Duis cursus tortor elit</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="profile_box">
+          <div class="top_curve">
+            <div class="profile_content">
+              <div class="pro_photo"><a href="#"><img src="images/pic_2.jpg" alt="" /></a></div>
+              <div class="outer">
+                <div class="pro_detail_box">
+                  <div class="pro_top_curve">
+                    <div class="pro_detail_content">
+                      <div class="row">
+                        <p class="left">Name :</p>
+                        <p class="right">Aaron</p>
+                      </div>
+                      <div class="row">
+                        <p class="left">Age :</p>
+                        <p class="right">21 Years</p>
+                      </div>
+                      <div class="row">
+                        <p class="left">Location :</p>
+                        <p class="right">FredVegas</p>
+                      </div>
+                      <div class="row last">
+                        <p class="left">Likes :</p>
+                        <p class="right">Programming, Costume parties</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="profile_box">
+          <div class="top_curve">
+            <div class="profile_content">
+              <div class="pro_photo"><a href="#"><img src="images/pic_3.jpg" alt="" /></a></div>
+              <div class="outer">
+                <div class="pro_detail_box">
+                  <div class="pro_top_curve">
+                    <div class="pro_detail_content">
+                      <div class="row">
+                        <p class="left">Name :</p>
+                        <p class="right">Shakira</p>
+                      </div>
+                      <div class="row">
+                        <p class="left">Age :</p>
+                        <p class="right">25 Years</p>
+                      </div>
+                      <div class="row">
+                        <p class="left">Location :</p>
+                        <p class="right">Argentina</p>
+                      </div>
+                      <div class="row last">
+                        <p class="left">Likes :</p>
+                        <p class="right">Duis cursus tortor elit</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="profile_box last">
+          <div class="top_curve">
+            <div class="profile_content">
+              <div class="pro_photo"><a href="#"><img src="images/pic_4.gif" alt="" /></a></div>
+              <div class="outer">
+                <div class="pro_detail_box">
+                  <div class="pro_top_curve">
+                    <div class="pro_detail_content">
+                      <div class="row">
+                        <p class="left">Name :</p>
+                        <p class="right">Jenifer</p>
+                      </div>
+                      <div class="row">
+                        <p class="left">Age :</p>
+                        <p class="right">23 Years</p>
+                      </div>
+                      <div class="row">
+                        <p class="left">Location :</p>
+                        <p class="right">Argentina</p>
+                      </div>
+                      <div class="row last">
+                        <p class="left">Likes :</p>
+                        <p class="right">Duis cursus tortor elit</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--recently profiles starts-->
+    </div>
+    <!--right container ends-->
+  </div>
+  <!--body container ends-->
+  <!--footer starts-->
+  <div id="footer"> <span class="footer_logo"><a href="#"><img src="images/single_2_mingle.gif" alt="" /></a></span>
+    <div class="footer_link">
+      <ul style="color:#FFF;">
+        Copyright (c) Sitename.com. All rights reserved. Design by Stylish <a style="color:#FFF; text-decoration:underline;" href="http://www.stylishtemplate.com">Website Templates</a>.
+      </ul>
+    </div>
+  </div>
+  <!--footer ends-->
+</div>
+<!--layout ends-->
+</body>
+</html>
