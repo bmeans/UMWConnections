41d7872b040964267a1ed0fe6e69dff296fd6886
Waleed Ershad <wershad@mail.umw.edu>
2012-03-15 14:38:29 -0400

    i am tweaking the advanced search page little by little

--- xhtml-css/advanced_search.php
+++ xhtml-css/advanced_search.php
@@ -13,95 +13,76 @@
   <!--header starts-->
   <div id="header">
     <!--menu starts-->
-    <div id="menu"> <span class="logo"><a href="http://localhost/UMWconnections/UMWConnections/xhtml-css/index.html"><img src="images/logo.gif" alt="" /></a></span>
-      <p>Loneliness is a disease and we are the cure.</p>
+    <div id="menu"> <span class="logo"><a href="#"><img src="images/logo.gif" alt="" /></a></span>
+      <p>Loneliness is a disease, we are the cure.</p>
       <ul>
         <li class="first"><a href="index.html">home</a></li>
-        <li><a href="about_us.html" class="about_us.html">about us</a></li>
+        <li><a href="about_us.html">about us</a></li>
         <li><a href="register.php">create account</a></li>
         <li><a href="my_profile.php">my profile</a></li>
         <li><a href="advanced_search.php">advanced search</a></li>
-        <li><a href="contact_us.php">contact Us</a></li>
+        <li><a class="current">contact us</a></li>
         <li><a href="logout.php">logout</a></li>
       </ul>
     </div>
     <!--menu ends-->
-    <!--<div id="banner_inner">
+    <div id="banner_inner">
+      <div class="find_love">
       
         <!--form container starts-->
         
-        
-     
-      <!--form container ends-->
-    
+    </div>
   </div>
-  -->
   <!--header ends-->
   <!--body container starts-->
   <div id="body_container">
     <!--left container starts-->
     <div id="left_container">
       <div style="padding:20px 15px 30px 15px;">
-      <h1><span>Advanced Search </span></h1>
-      <div>
-      <!--<strong> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin  sed odio et ante adipiscing lobortis. Quisque eleifend, arcu a dictum  varius, risus neque venenatis arcu, a semper massa mi eget ipsum. </strong> <br />
-        -->
+      <h1><span>Advanced Search</span></h1>
+      <div> <br />
+        <h6 class="inner">Get more picky by using advanced search:</h6>
         <br />
-        <?php
-		?>	
+        <form>
+          <table width="80%">
+            <tr>
+              <td align="left" valign="top" class="body" id="ImA"><strong>I am a</strong></td>
+              <td align="left" valign="top"><select name="gender" class = "gender"><option>Male</option><option>Female</option></select></td>
+            </tr>
+            <tr>
+              <td align="left" valign="top" class="body" id="Email"><strong> Email: </strong></td>
+              <td align="left" valign="top"><input name="Email" type="text" size="40" /></td>
+            </tr>
+            <tr>
+              <td align="left" valign="top" class="body" id="Comments"><strong> Questions / Comments: </strong></td>
+              <td align="left" valign="top"><textarea name="comments" cols="32" rows="6"></textarea></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td><input type="submit" name="submit" class="button" value="Send Now" /></td>
+            </tr>
+          </table>
+        </form>
+      </div>
+      <div> <br />
+       <!-- <h6 class="inner">Contact Information: </h6>
+        <img src="images/photo-contact.jpg" alt="" width="152" height="100" class="project-img" /><br />
         <br />
-      <div class="clear"></div>
-      <div class="clear"></div>
+        <br />
+        100 Lorem Ipsum lor Sit<br />
+        88-99 Sit Amet, Lorem<br />
+        USA<br />
+        <br />
+        <p> <span><img src="images/ico-phone.png" alt="Phone" width="20" height="16" hspace="2" /> Phone:</span> (888) 123 456 789<br />
+          <span><img src="images/ico-fax.png" alt="Fax" width="20" height="16" hspace="2" /> Fax:</span> (888) 987 654 321<br />
+          <span><img src="images/ico-website.png" alt="WWW Link" width="20" height="16" hspace="2" /> Website:</span> <a href="#">www.mycompany.com</a><br />
+          <span><img src="images/ico-email.png" alt="Email" width="20" height="16" hspace="2" /> Email:</span> <a href="mailto:info@mycompany.com">info@mycompany.com</a><br />
+          <span><img src="images/ico-twitter.png" alt="Twitter Follow" width="20" height="16" hspace="3" /> <a href="#">Follow</a> on Twitter</span><br />
+        </p> --!>
       </div>
-      </div>
-      </div>
-      <!--
-      <div class="servicecolumnzone">
-        <div class="servicecolumn1">
-          <div>
-            <h5 class="inner"> Service 1</h5>
-            <img src="images/ico1.png" alt="" class="abouticon" />Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin sed  odio et ante adipiscing lobortis. Quisque eleifend, arcu a dictum  varius, </div>
-          <div class="clear"></div>
-        </div>
-        <div class="servicecolumn2">
-          <div>
-            <h5 class="inner">Service 2</h5>
-            <img src="images/ico2.png" alt="" width="65" height="65" class="abouticon" />Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin sed  odio et ante adipiscing lobortis. Quisque eleifend, arcu a dictum  varius, </div>
-          <div class="clear"></div>
-        </div>
-        <div class="clear"></div>
-        <div class="servicecolumn1">
-          <div>
-            <h5 class="inner">Service 3 </h5>
-            <img src="images/ico3.png" alt="" width="65" height="65" class="abouticon" />Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin sed  odio et ante adipiscing lobortis. Quisque eleifend, arcu a dictum  varius, </div>
-          <div class="clear"></div>
-        </div>
-        <div class="servicecolumn2">
-          <div>
-            <h5 class="inner">Service 4 </h5>
-            <img src="images/ico4.png" alt="" width="65" height="65" class="abouticon" />Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin sed  odio et ante adipiscing lobortis. Quisque eleifend, arcu a dictum  varius, </div>
-          <div class="clear"></div>
-        </div>
-        <div class="clear"></div>
-        <div class="servicecolumn1">
-          <div>
-            <h5 class="inner">Service 5 </h5>
-            <img src="images/ico5.png" alt="" width="65" height="65" class="abouticon" />Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin sed  odio et ante adipiscing lobortis. Quisque eleifend, arcu a dictum  varius, </div>
-          <div class="clear"></div>
-        </div>
-        <div class="servicecolumn2">
-          <div>
-            <h5 class="inner">Service 6 </h5>
-            <img src="images/ico6.png" alt="" width="65" height="65" class="abouticon" />Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin sed  odio et ante adipiscing lobortis. Quisque eleifend, arcu a dictum  varius, </div>
-          <div class="clear"></div>
-        </div>
-        <div class="clear"></div>
-      </div>
-      <div style="padding-top:10px;">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin sed  odio et ante adipiscing lobortis. Quisque eleifend, arcu a dictum  varius, risus neque venenatis arcu, a semper massa mi eget ipsum. Proin  sed odio et ante adipiscing lobortis. Lorem ipsum dolor sit amet,  consectetuer adipiscing elit. Proin sed odio et ante adipiscing  lobortis. Quisque eleifend, arcu a dictum varius, risus neque venenatis  arcu, a semper massa mi eget ipsum. Proin sed odio et ante adipiscing  lobortis.</div>
     </div>
       <div class="clear"></div>
     </div>
-    -->
     <!--left container ends-->
     <!--right container starts-->
     <div id="right_container">
@@ -111,26 +92,26 @@
         <div class="profile_box">
           <div class="top_curve">
             <div class="profile_content">
-              <div class="pro_photo"><a href="#"><img src="images/pic_1.png" alt="" /></a></div>
+              <div class="pro_photo"><a href="#"><img src="images/pic_1.gif" alt="" /></a></div>
               <div class="outer">
                 <div class="pro_detail_box">
                   <div class="pro_top_curve">
                     <div class="pro_detail_content">
                       <div class="row">
                         <p class="left">Name :</p>
-                        <p class="right">Nelly</p>
+                        <p class="right">Jenifer</p>
                       </div>
                       <div class="row">
                         <p class="left">Age :</p>
-                        <p class="right">20 Years</p>
+                        <p class="right">23 Years</p>
                       </div>
                       <div class="row">
                         <p class="left">Location :</p>
-                        <p class="right">Orange County</p>
+                        <p class="right">South Korea</p>
                       </div>
                       <div class="row last">
                         <p class="left">Likes :</p>
-                        <p class="right">sleeping</p>
+                        <p class="right">Duis cursus tortor elit</p>
                       </div>
                     </div>
                   </div>
@@ -149,19 +130,19 @@
                     <div class="pro_detail_content">
                       <div class="row">
                         <p class="left">Name :</p>
-                        <p class="right">Aaron</p>
+                        <p class="right">David Martin</p>
                       </div>
                       <div class="row">
                         <p class="left">Age :</p>
-                        <p class="right">21 Years</p>
+                        <p class="right">28 Years</p>
                       </div>
                       <div class="row">
                         <p class="left">Location :</p>
-                        <p class="right">Fredvegas</p>
+                        <p class="right">England</p>
                       </div>
                       <div class="row last">
                         <p class="left">Likes :</p>
-                        <p class="right">programming, costume parties</p>
+                        <p class="right">Duis cursus tortor elit</p>
                       </div>
                     </div>
                   </div>
@@ -192,7 +173,7 @@
                       </div>
                       <div class="row last">
                         <p class="left">Likes :</p>
-                        <p class="right">eating</p>
+                        <p class="right">Duis cursus tortor elit</p>
                       </div>
                     </div>
                   </div>
@@ -204,18 +185,18 @@
         <div class="profile_box last">
           <div class="top_curve">
             <div class="profile_content">
-              <div class="pro_photo"><a href="#"><img src="images/pic_4.png" alt="" /></a></div>
+              <div class="pro_photo"><a href="#"><img src="images/pic_4.gif" alt="" /></a></div>
               <div class="outer">
                 <div class="pro_detail_box">
                   <div class="pro_top_curve">
                     <div class="pro_detail_content">
                       <div class="row">
                         <p class="left">Name :</p>
-                        <p class="right">Keith</p>
+                        <p class="right">Jenifer</p>
                       </div>
                       <div class="row">
                         <p class="left">Age :</p>
-                        <p class="right">32 Years</p>
+                        <p class="right">23 Years</p>
                       </div>
                       <div class="row">
                         <p class="left">Location :</p>
@@ -223,7 +204,7 @@
                       </div>
                       <div class="row last">
                         <p class="left">Likes :</p>
-                        <p class="right">Watching anime, skyping with pretty ladies</p>
+                        <p class="right">Duis cursus tortor elit</p>
                       </div>
                     </div>
                   </div>
@@ -241,7 +222,7 @@
   <!--footer starts-->
   <div id="footer"> <span class="footer_logo"><a href="#"><img src="images/single_2_mingle.gif" alt="" /></a></span>
     <div class="footer_link">
-        <ul style="color:#FFF;">
+      <ul style="color:#FFF;">
         Copyright (c) Sitename.com. All rights reserved. Design by Stylish <a style="color:#FFF; text-decoration:underline;" href="http://www.stylishtemplate.com">Website Templates</a>.
       </ul>
     </div>
