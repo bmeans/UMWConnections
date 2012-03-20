60296768645dbd028f89111da823f66c2f79fafc
Aaron537 <Aaron.h.crowe@gmail.com>
2012-03-19 21:04:02 -0300

    Update xhtml-css/edit_profile.php

--- xhtml-css/edit_profile.php
+++ xhtml-css/edit_profile.php
@@ -1,3 +1,7 @@
+<?php
+session_start()
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -102,69 +106,29 @@
     <!--left container starts-->
     <div id="left_container">
      <div style="padding:20px 15px 30px 35px;">
-      <h1><span>Privacy </span>Policy</h1>
-      <div style="float:right; width:auto"><img src="images/ico-terms.png" alt="" /></div>
-      <div> <strong> <br />
-        What information do we collect?</strong><br />
-        We collect  information from you when you register on our site, place an order,  subscribe to our newsletter, respond to a survey or fill out a form. <br />
-        When  ordering or registering on our site, as appropriate, you may be asked  to enter your: name, e-mail address, mailing address, phone number,  credit card information or social security number. You may, however,  visit our site anonymously.<br />
-        <br />
-        <strong>What do we use your information for?</strong> <br />
-        Any of the information we collect from you may be used in one of the following ways: <br />
-        <ul style="padding-left:15px; margin:0px;">
-          <li>To personalize your experience<br />
-            (your information helps us to better respond to your individual needs)</li>
-          <li>To improve our website<br />
-            (we continually strive to improve our website offerings based on the information and feedback we receive from you)</li>
-          <li> To improve customer service<br />
-            (your information helps us to more effectively respond to your customer service requests and support needs)</li>
-          <li>To process transactions<br />
-            We may share or sell information with third parties for marketing or other purposes without your consent.</li>
-          <li>To administer a contest, promotion, survey or other site feature</li>
-          <li>To send periodic emails<br />
-            The  email address you provide for order processing, may be used to send you  information and updates pertaining to your order, in addition to  receiving occasional company news, updates, related product or service  information, etc.</li>
-        </ul>
-        Note: If at any time you would like to  unsubscribe from receiving future emails, we include detailed  unsubscribe instructions at the bottom of each email.<br />
-        <br />
-        <strong>How do we protect your information?</strong><br />
-        We  implement a variety of security measures to maintain the safety of your  personal information when you place an order or access your personal  information. <br />
-        We offer the use of a secure server. All  supplied sensitive/credit information is transmitted via Secure Socket  Layer (SSL) technology and then encrypted into our Payment gateway  providers database only to be accessible by those authorized with  special access rights to such systems, and are required to&iuml;&iquest;&frac12;keep the  information confidential. <br />
-        After a transaction, your private  information (credit cards, social security numbers, financials, etc.)  will not be kept on file for more than 60 days.<br />
-        <br />
-        <strong>Do we use cookies?</strong> <br />
-        Yes  (Cookies are small files that a site or its service provider transfers  to your computers hard drive through your Web browser (if you allow)  that enables the sites or service providers systems to recognize your  browser and capture and remember certain information <br />
-        We use  cookies to help us remember and process the items in your shopping  cart, understand and save your preferences for future visits, keep  track of advertisements and compile aggregate data about site traffic  and site interaction so that we can offer better site experiences and  tools in the future. We may contract with third-party service providers  to assist us in better understanding our site visitors. These service  providers are not permitted to use the information collected on our  behalf except to help us conduct and improve our business. <br />
-        If  you prefer, you can choose to have your computer warn you each time a  cookie is being sent, or you can choose to turn off all cookies via  your browser settings. Like most websites, if you turn your cookies  off, some of our services may not function properly. However, you can  still place orders over the telephone or by contacting customer service.<br />
-        <br />
-        <strong>Do we disclose any information to outside parties?</strong><br />
-        We  do not sell, trade, or otherwise transfer to outside parties your  personally identifiable information. This does not include trusted  third parties who assist us in operating our website, conducting our  business, or servicing you, so long as those parties agree to keep this  information confidential. We may also release your information when we  believe release is appropriate to comply with the law, enforce our site  policies, or protect ours or others rights, property, or safety.  However, non-personally identifiable visitor information may be  provided to other parties for marketing, advertising, or other uses.<br />
-        <br />
-        <strong>Third party links</strong><br />
-        Occasionally, at our discretion, we may include or offer third party  products or services on our website. These third party sites have  separate and independent privacy policies. We therefore have no  responsibility or liability for the content and activities of these  linked sites. Nonetheless, we seek to protect the integrity of our site  and welcome any feedback about these sites.<br />
-        <br />
-        <strong>California Online Privacy Protection Act Compliance</strong><br />
-        Because  we value your privacy we have taken the necessary precautions to be in  compliance with the California Online Privacy Protection Act. We  therefore will not distribute your personal information to outside  parties without your consent.<br />
-        <br />
-        <strong>Terms and Conditions</strong><br />
-        Please also visit our Terms and Conditions section establishing the  use, disclaimers, and limitations of liability governing the use of our  website at <strong>http://www.MYCOMPANY.com</strong> (replace with yours)<br />
-        <br />
-        <strong>Your Consent</strong><br />
-        By using our site, you consent to our <a href="#" target="_blank">websites privacy policy</a>.<br />
-        <br />
-        <strong>Changes to our Privacy Policy</strong><br />
-        If we decide to change our privacy policy, we will post those changes on this page. <br />
-        <br />
-        <strong>Contacting Us</strong> <br />
-        If there are any questions regarding this privacy policy you may contact us using the information below. <br />
-        <br />
-        MYCOMPANY.COM<br />
-        123 ADDRESS STREET<br />
-        MY CITY, MYSTATE 76543<br />
-        USA<br />
-            EMAIL@MYCOMPANY.COM<br /><hr noshade="noshade" size="1" color="#ccc" /><small>Privacy Policy 
-
-prepared by <a href="http://www.dreamtemplate.com" alt="Website 
-
+      <form name="input" action="editProfileUpdate.php" method="post">
+       First Name: <input type="text" value="<?php echo $_SESSION['first_name'] ?>" name="first_name" /><br />
+       Last Name: <input type="text" value="<?php echo $_SESSION['last_name'] ?>" name = "last_name" /><br />
+       Gender: <input type="radio" name="gender" value="male" />Male    <input type="radio" name="gender" value"female" />Female<br />
+       Major: <input type="text" value="<?php echo $_SESSION['major'] ?>" name="major" /><br />
+       Phone Number: <textarea rows="1" cols="10"><?php echo $_SESSION['phone'] ?></textarea><small><I>Don't include dashes or spaces</I></small><br /><br />
+       What are you looking for?<br />
+       <input type="checkbox" name="looking_for" value="date" />Dates<br />
+       <input type="checkbox" name="looking_for" value="relationship" /> A Relationship<br />
+       <input type="checkbox" name="looking_for" value="friendship" />Friendship<br />
+       <input type="checkbox" name="looking_for" value="study_group" />Study Groups<br />
+       <input type="checkbox" name="looking_for" value="sports" />People to play sports with<br /><br />
+       Who are you looking for?<br />
+       <input type="radio" name="interested_in" value="male" />Men    <input type="radio" name="interested_in" value="female" />Women    <input type="radio" name="interested_in" value="no_preference" />No Preference<br /><br />
+       What do you have to say about yourself?<br />
+       <textarea rows="5" cols="40"><?php echo $_SESSION['description'] ?></textarea><br /><br />
+       What are your interests?<br />
+       <textarea rows="5" cols="40"><?php echo $_SESSION['interests'] ?></textarea><br />
+       <input type="submit" value="Submit" />
+       </form>
+       
+       
+       
 Templates">DreamTemplate</a></small> </div>
     </div>
     </div>
