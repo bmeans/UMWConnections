1b8f4d2afca821081f46e23b73f11db6fcc32771
Aaron537 <Aaron.h.crowe@gmail.com>
2012-03-28 22:17:00 -0300

    Update xhtml-css/update_profile.php

--- xhtml-css/update_profile.php
+++ xhtml-css/update_profile.php
@@ -16,7 +16,13 @@ session_start()
  $gender = $_POST['gender'];
  $phone = $_POST['phone'];
  $description = $_POST['description'];
+ $major = $_POST['major'];
+ $looking = $_POST['looking_for'];
+ $interestedIn = $_POST['interested_in'];
+ $interests = $_POST['interests'];
  $query = "UPDATE Users SET first_name='$first_name', last_name='$last_name', gender='$gender', phone='$phone', description='$description' where email='$email'";
+ #$interestsQuery = "UPDATE Interests, Users, Users_Interests SET interest='$interests' where "
+ #$majorQuery
  mysqli_query($db, $query);
  
  ?>
